d4c1caa0764dda59db3c809de91738f0d62e3c40
jooyoung0525 <pjuy0525@naver.com>
2021-09-27 21:39:08 +0900

    Update (test문제)배열의최댓값.java

--- Algorithm/BaekJoon/(test문제)배열의최댓값.java
+++ Algorithm/BaekJoon/(test문제)배열의최댓값.java
@@ -1,7 +1,5 @@
-package naver;
 
-
-public class cloud1 {
+public class problem1 {
 public static void main(String[] args) {
 	int costs[][] = {{1,2,3},{4,5,6},{7,8,9}};
 	//int costs[][] = {{0,0,0},{0,0,0},{0,0,1}};
